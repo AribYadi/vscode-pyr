c3234fb0a329ed5f05afe3e32f4847a1ea17a97e
AribYadi <aribyadi@gmail.com>
2022-06-10 18:25:13 +0700

    fix: disable semantic highlighting (for now)

--- src/extension.ts
+++ src/extension.ts
@@ -1,140 +1,145 @@
 import * as vscode from "vscode";
 
-const tokenTypes = new Map<string, number>;
-const tokenMods = new Map<string, number>;
-const legend = (function () {
-  const tokenTypesLegend: string[] = [
-    'variable', 'operator'
-  ];
-  tokenTypesLegend.forEach((tokenType, i) => tokenTypes.set(tokenType, i));
+// const tokenTypes = new Map<string, number>;
+// const tokenMods = new Map<string, number>;
+// const legend = (function () {
+//   const tokenTypesLegend: string[] = [
+//     'variable', 'operator'
+//   ];
+//   tokenTypesLegend.forEach((tokenType, i) => tokenTypes.set(tokenType, i));
 
-  const tokenModsLegend: string[] = [];
-  tokenModsLegend.forEach((tokenMod, i) => tokenMods.set(tokenMod, i));
+//   const tokenModsLegend: string[] = [];
+//   tokenModsLegend.forEach((tokenMod, i) => tokenMods.set(tokenMod, i));
 
-  return new vscode.SemanticTokensLegend(tokenTypesLegend, tokenModsLegend);
-})();
+//   return new vscode.SemanticTokensLegend(tokenTypesLegend, tokenModsLegend);
+// })();
 
 export function activate(ctx: vscode.ExtensionContext) {
-  const completionItemProvider = vscode.languages.registerCompletionItemProvider({ scheme: 'file', language: 'pyrlang' }, new CompletionItemProvider(), '\n');
+  const completionItemProvider =
+    vscode.languages.registerCompletionItemProvider(
+      { scheme: "file", language: "pyrlang" },
+      new CompletionItemProvider(),
+      "\n"
+    );
   ctx.subscriptions.push(completionItemProvider);
-  const documentSemanticTokensProvider = vscode.languages.registerDocumentSemanticTokensProvider({ scheme: 'file', language: 'pyrlang' }, new DocumentSemanticTokensProvider(), legend);
-  ctx.subscriptions.push(documentSemanticTokensProvider);
+  // const documentSemanticTokensProvider = vscode.languages.registerDocumentSemanticTokensProvider({ scheme: 'file', language: 'pyrlang' }, new DocumentSemanticTokensProvider(), legend);
+  // ctx.subscriptions.push(documentSemanticTokensProvider);
 }
 
 class CompletionItemProvider implements vscode.CompletionItemProvider {
-  provideCompletionItems(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken, context: vscode.CompletionContext): vscode.ProviderResult<vscode.CompletionItem[] | vscode.CompletionList<vscode.CompletionItem>> {
-    const if_ = new vscode.CompletionItem('if');
-    if_.insertText = new vscode.SnippetString('if ${1:condition}:\n\t${2:body}');
-    const else_ = new vscode.CompletionItem('else');
-    else_.insertText = new vscode.SnippetString('else:\n\t${1:body}');
-    const while_ = new vscode.CompletionItem('while');
-    while_.insertText = new vscode.SnippetString('while ${1:condition}:\n\t${2:body}');
-    const print = new vscode.CompletionItem('print');
-    print.insertText = new vscode.SnippetString('print ${1:value}');
+  provideCompletionItems(
+    document: vscode.TextDocument,
+    position: vscode.Position,
+    token: vscode.CancellationToken,
+    context: vscode.CompletionContext
+  ): vscode.ProviderResult<
+    vscode.CompletionItem[] | vscode.CompletionList<vscode.CompletionItem>
+  > {
+    const if_ = new vscode.CompletionItem("if");
+    if_.insertText = new vscode.SnippetString(
+      "if ${1:condition}:\n\t${2:body}"
+    );
+    const else_ = new vscode.CompletionItem("else");
+    else_.insertText = new vscode.SnippetString("else:\n\t${1:body}");
+    const while_ = new vscode.CompletionItem("while");
+    while_.insertText = new vscode.SnippetString(
+      "while ${1:condition}:\n\t${2:body}"
+    );
+    const print = new vscode.CompletionItem("print");
+    print.insertText = new vscode.SnippetString("print ${1:value}");
 
     return [if_, else_, while_, print];
   }
 }
 
-interface IToken {
-  line: number;
-  startCharacter: number;
-	length: number;
-	tokenType: string;
-	tokenModifiers: string[];
-}
+// interface IToken {
+//   line: number;
+//   startCharacter: number;
+// 	length: number;
+// 	tokenType: string;
+// 	tokenModifiers: string[];
+// }
 
-class DocumentSemanticTokensProvider implements vscode.DocumentSemanticTokensProvider {
-  async provideDocumentSemanticTokens(document: vscode.TextDocument, token: vscode.CancellationToken): Promise<vscode.SemanticTokens> {
-    const allTokens = this._parseText(document.getText());
-    const builder = new vscode.SemanticTokensBuilder();
-    allTokens.forEach((token) => {
-      builder.push(token.line, token.startCharacter, token.length, this._encodeTokenType(token.tokenType), this._encodeTokenModifiers(token.tokenModifiers));
-    });
-    return builder.build();
-  }
+// class DocumentSemanticTokensProvider implements vscode.DocumentSemanticTokensProvider {
+//   async provideDocumentSemanticTokens(document: vscode.TextDocument, token: vscode.CancellationToken): Promise<vscode.SemanticTokens> {
+//     const allTokens = this._parseText(document.getText());
+//     const builder = new vscode.SemanticTokensBuilder();
+//     allTokens.forEach((token) => {
+//       builder.push(token.line, token.startCharacter, token.length, this._encodeTokenType(token.tokenType), this._encodeTokenModifiers(token.tokenModifiers));
+//     });
+//     return builder.build();
+//   }
 
-  private _encodeTokenType(tokenType: string): number {
-    if (tokenTypes.has(tokenType)) return tokenTypes.get(tokenType)!;
-    return 0;
-  }
+//   private _encodeTokenType(tokenType: string): number {
+//     if (tokenTypes.has(tokenType)) return tokenTypes.get(tokenType)!;
+//     return 0;
+//   }
 
-  private _encodeTokenModifiers(strTokenMods: string[]): number {
-    let result = 0;
-		for (const tokenModifier of strTokenMods) {
-			if (tokenMods.has(tokenModifier)) {
-				result = result | (1 << tokenMods.get(tokenModifier)!);
-			}
-    }
-		return result;
-  }
+//   private _encodeTokenModifiers(strTokenMods: string[]): number {
+//     let result = 0;
+// 		for (const tokenModifier of strTokenMods) {
+// 			if (tokenMods.has(tokenModifier)) {
+// 				result = result | (1 << tokenMods.get(tokenModifier)!);
+// 			}
+//     }
+// 		return result;
+//   }
 
-  private _parseText(text: string): IToken[] {
-    const out: IToken[] = [];
-    const lines = text.split(/\r\n|\r|\n/);
-    
-    let inString = false;
-    for (let i = 0; i < lines.length; i++) {
-      const line = lines[i];
-      let column = 0;
-      let currentWord = '';
-      do {
-        currentWord += line[column];
-        column++;
+//   private _parseText(text: string): IToken[] {
+//     const out: IToken[] = [];
+//     const lines = text.split(/\r\n|\r|\n/);
 
-        let tokenType: string | undefined;
+//     let inString = false;
+//     for (let i = 0; i < lines.length; i++) {
+//       const line = lines[i];
+//       let column = 0;
+//       let currentWord = '';
+//       do {
+//         if (line[column] === '"' && ((inString && line[column - 1] !== '\\') || true)) inString = !inString;
+//         while (!inString && !/[a-zA-Z0-9"]|/.test(line[column])) column++;
+//         if (line[column] === '#') {
+//           do column++;
+//           while (column < line.length);
+//         }
 
-        switch (currentWord) {
-          case 'if':
-          case 'else':
-          case 'while':
-          case 'print':
-          case 'true':
-          case 'false':
-          case 'and':
-          case 'or':
-            if (!/[a-zA-Z0-9_]/.test(line[column])) currentWord = '';
-            break;
+//         currentWord += line[column];
+//         column++;
 
-          default:
-              if (!inString && currentWord.length > 0 && /[a-zA-Z_][a-zA-Z0-9_]*/.test(currentWord) && (column > line.length - 1 || !/[a-zA-Z_][a-zA-Z0-9_]*/.test(line[column]))) {
-                tokenType = 'variable';
-                break;
-            }
-          break;
-        }
+//         let tokenType: string | undefined;
 
-        if (currentWord.startsWith('"')) {
-          inString = true;
-          if (currentWord.length > 1 && !currentWord.endsWith('\\"') && currentWord.endsWith('"')) {
-            inString = false;
-            tokenType = undefined;
-          }
-        }
+//         switch (currentWord) {
+//           case 'if':
+//           case 'else':
+//           case 'while':
+//           case 'print':
+//           case 'true':
+//           case 'false':
+//           case 'and':
+//           case 'or':
+//             if (!/[a-zA-Z0-9_]/.test(line[column])) currentWord = '';
+//             break;
 
-        if (tokenType !== undefined) {
-          out.push({
-            line: i,
-            startCharacter: column - currentWord.length,
-            length: currentWord.length,
-            tokenType: tokenType!,
-            tokenModifiers: []
-          });
-          currentWord = '';
-        }
+//           default:
+//               if (!inString && currentWord.length > 0 && /[a-zA-Z_][a-zA-Z0-9_]*/.test(currentWord) && (column > line.length - 1 || !/[a-zA-Z_][a-zA-Z0-9_]*/.test(line[column]))) {
+//                 tokenType = 'variable';
+//                 break;
+//             }
+//           break;
+//         }
 
-        if (/[ \t]/.test(line[column]) && !inString) {
-          currentWord = '';
-          column++;
-        }
-        if (line[column] === '#') {
-          while (column < line.length || line[column] !== '\n') {
-            column++;
-          }
-        }
-      } while (column < line.length);
-    }
+//         if (tokenType !== undefined) {
+//           out.push({
+//             line: i,
+//             startCharacter: column - currentWord.length,
+//             length: currentWord.length,
+//             tokenType: tokenType!,
+//             tokenModifiers: []
+//           });
+//           currentWord = '';
+//         }
+//       } while (column < line.length);
+//     }
 
-    return out;
-  }
-}
+//     return out;
+//   }
+// }
